fc9f60391b42fdcedc7305e95b57dde1dea3c60a
Daniel <bluh@outlook.com>
2018-11-27 22:50:14 -0500

    adds user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,26 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    if auth
+      user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+      end
+    else
+      user = User.new(user_params)
+      if user && user.save
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        flash[:notice] = "Something Went Wrong Creating This User. Maybe one with this username already exists?"
+        redirect_to '/users/new'
+      end
+    end
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
+
 
   private
 
