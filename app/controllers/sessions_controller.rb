9ca2f67c52857e817ebe9b242bd665a480aa191e
Daniel <bluh@outlook.com>
2018-11-27 23:23:09 -0500

    added session routes + session destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def destroy
+    session.delete(:user_id) unless session[:user_id].nil?
+    redirect_to '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
   resources :users
+  get '/login', to: 'sessions#new'
+  resources :sessions, only: [:create]
+  delete '/logout', to: 'sessions#destroy'
 end
