83e33f5415007b3b4e15677c1b013c2d8de52618
Daniel <bluh@outlook.com>
2018-11-27 22:36:10 -0500

    added user_params helper method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,8 @@ class UsersController < ApplicationController
   def auth
     request.env['omniauth.auth']
   end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :max_calories)
+  end
 end
