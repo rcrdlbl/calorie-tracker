6d1205c1eec7377a849406b8ab3453bbe4eb88ef
Daniel <bluh@outlook.com>
2018-11-28 19:33:28 -0500

    added create session action to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    if auth
+      user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+      end
+      redirect_to user_path(user)
+    else
+      user = User.find_by(email: params[:user][:email])
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        flash[:notice] = "Something went wrong signing in. Maybe your password is misspelled?"
+        redirect_to '/signin'
+      end
+    end
   end
 
   def destroy
@@ -13,5 +28,8 @@ class SessionsController < ApplicationController
 
   private
 
-  
+  def auth
+    request.env['omniauth.auth']
+  end
+
 end
