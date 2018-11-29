b8632329c25161d549f3756d315d03805c3c4f7e
Daniel <bluh@outlook.com>
2018-11-28 19:26:49 -0500

    Added login/signup links to front page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,16 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+
+  end
+
   def destroy
     session.delete(:user_id) unless session[:user_id].nil?
     redirect_to '/'
   end
+
+  private
+
+  
 end
