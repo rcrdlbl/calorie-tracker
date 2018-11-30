80d01ffee83d46de782d8c84f910cfb2752c04a0
Daniel <bluh@outlook.com>
2018-11-30 00:32:13 -0500

    added edit action to food items controller

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -11,8 +11,17 @@ class FoodItemsController < ApplicationController
   end
 
   def create
-    food_item = FoodItem.create(food_item_params)
-    redirect_to food_item_path(food_item)
+    food_item = FoodItem.new(food_item_params)
+    if food_item && food_item.save
+      redirect_to food_item_path(food_item)
+    else
+      flash[:notice] = "Something went wrong creating this food item. Did you fill out all the fields?"
+      redirect_to new_food_item_path
+    end
+  end
+
+  def edit
+    @food_item = FoodItem.find(params[:id])
   end
 
 
