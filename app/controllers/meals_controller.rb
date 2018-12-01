3e24cd8f2d0949e7dc6c5083d0976aed06b1d305
Daniel <bluh@outlook.com>
2018-11-30 21:44:32 -0500

    added create method and params method to meals controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,4 +2,13 @@ class MealsController < ApplicationController
   def new
     @meal = Meal.new(food_item_id: params[:food_item_id], user_id: session[:user_id] )
   end
+
+  def create
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:description, :food_quantity, :user_id, :food_item_id)
+  end
 end
