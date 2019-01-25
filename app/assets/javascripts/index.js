16d1c0de25232d451f535476509665e42b60e97c
rcrdlbl <bluh@outlook.com>
2019-01-24 19:56:06 -0500

    getFoodItem(), getMeal()

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -18,6 +18,38 @@ function getUser() {
 
 	})
 }
+function getFoodItem() {
+	$.ajax({
+		url: "http://localhost:3000/food_items/1",
+		method: 'get',
+		dataType: 'json'
+	}).done(function (response) {
+		console.log("response: ", response);
+
+
+		let user = new FoodItem(response)
+
+		// let userDisplay = user.userHTML()
+    // return user
+
+	})
+}
+function getMeal() {
+	$.ajax({
+		url: "http://localhost:3000/meals/1",
+		method: 'get',
+		dataType: 'json'
+	}).done(function (response) {
+		console.log("response: ", response);
+
+
+		let user = new Meal(response)
+
+		// let userDisplay = user.userHTML()
+    // return user
+
+	})
+}
 getUser()
 
 class User {
