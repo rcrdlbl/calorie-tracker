00ee2a75beeb0957b5966566151b9ee9bb16360a
rcrdlbl <bluh@outlook.com>
2019-01-24 21:08:23 -0500

    fixed get functions and added HTML prototypes

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,57 +1,56 @@
-$(function () {
-	console.log('index.js loaded ... ');
+$(function() {
+  console.log('index.js loaded ... ');
 })
-
+// Get model functions
 function getUser() {
-	$.ajax({
-		url: "http://localhost:3000/users/1",
-		method: 'get',
-		dataType: 'json'
-	}).done(function (response) {
-		console.log("response: ", response);
+  $.ajax({
+    url: "http://localhost:3000/users/1",
+    method: 'get',
+    dataType: 'json'
+  }).done(function(response) {
+    console.log("response: ", response);
 
 
-		let user = new User(response)
-
-		// let userDisplay = user.userHTML()
+    let user = new User(response)
+    let userDisplay = user.userHTML()
     // return user
+    // $('div.main').html(userDisplay)
 
-	})
+  })
 }
+
 function getFoodItem() {
-	$.ajax({
-		url: "http://localhost:3000/food_items/1",
-		method: 'get',
-		dataType: 'json'
-	}).done(function (response) {
-		console.log("response: ", response);
+  $.ajax({
+    url: "http://localhost:3000/food_items/1",
+    method: 'get',
+    dataType: 'json'
+  }).done(function(response) {
+    console.log("response: ", response);
 
 
-		let user = new FoodItem(response)
+    let food_item = new FoodItem(response)
 
-		// let userDisplay = user.userHTML()
-    // return user
 
-	})
+  })
 }
+
 function getMeal() {
-	$.ajax({
-		url: "http://localhost:3000/meals/1",
-		method: 'get',
-		dataType: 'json'
-	}).done(function (response) {
-		console.log("response: ", response);
+  $.ajax({
+    url: "http://localhost:3000/meals/1",
+    method: 'get',
+    dataType: 'json'
+  }).done(function(response) {
+    console.log("response: ", response);
 
 
-		let user = new Meal(response)
+    let meal = new Meal(response)
 
-		// let userDisplay = user.userHTML()
-    // return user
 
-	})
+  })
 }
 getUser()
 
+// Model classes
 class User {
   constructor(obj) {
     this.name = obj.name
@@ -59,4 +58,34 @@ class User {
     this.id = obj.id
     this.max_calories = obj.max_calories
   }
+}
+
+User.prototype.userHTML = function () {
+  return (`
+    <div>
+    <h1>${this.name}</h1>
+    </div>
+  `)
+}
+
+class Meal {
+  constructor(obj) {
+    this.description = obj.description
+    this.food_quantity = obj.food_quantity
+  }
+}
+
+class FoodItem {
+  constructor(obj) {
+    this.name = obj.name
+    this.calories = obj.calories
+  }
+}
+
+FoodItem.prototype.foodItemHTML = function () {
+  return(`
+      <div>
+      <h4>${this.name} - ${this.calories} calories per serving</h4>
+      </div>
+    `)
 }
